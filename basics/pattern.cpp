e9bf1831fa4a6510c7b923e76fcb820e26eb4990
Ishant-codev <ishant.alok007@gmail.com>
2026-07-14 16:56:28 +0000

    ok

--- basics/pattern.cpp
+++ basics/pattern.cpp
@@ -135,4 +135,106 @@
 //     }
 // }
 
+//Binary triangle
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+//     for(int i=1;i<=n;i++){
+//         for(int j=1;j<=i;j++){
+//             if((i+j)%2==0) cout<<1<<" ";
+//             else cout<<0<<" ";
+//         }
+//         cout<<endl;
+//     }
+// }
+
+//star triangle vertically flipped
+//method-1
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+//     for(int i=1;i<=n;i++){
+//         for(int j=1;j<=n;j++){
+//             if((i+j>n)) cout<<"*";
+//             else cout<<" "; 
+//         }
+//         cout<<endl;
+//     }
+// }
+
+//method-2
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+//     for(int i=1;i<=n;i++){
+//         for(int j=1;j<=n-i;j++){ //n+1-i ki jaga n-1 krdiya
+//             cout<<" ";
+//         }
+//         for(int j=1;j<=i;j++){
+//             cout<<"*";
+//         }
+//         cout<<endl;
+//         }
+// }
+
+// star rhombus
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+//     for(int i=1;i<=n;i++){
+//         for(int j=1;j<=n-i;j++){ //n+1-i ki jaga n-1 krdiya
+//             cout<<" ";
+//         }
+//         for(int j=1;j<=n;j++){
+//             cout<<"*";
+//         }
+//         cout<<endl;
+//         }
+// }
+
+//Pyramid
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+//     for(int i=1;i<=n;i++){
+//         for(int j=1;j<=n-i;j++){
+//             cout<<" ";
+//         }
+//         for(int j=1;j<=2*i-1;j++){
+//             cout<<"*";
+//         }
+//         cout<<endl;
+//     }
+// }
+
+//ulta pyramid
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+//     int nst=2*n-1,nsp=0;
+//     for(int i=1;i<=n;i++){
+//         for(int j=1;j<=nsp;j++){
+//             cout<<" ";
+//         }
+//         for(int j=1;j<=nst;j++){
+//             cout<<"*";
+//         }
+//         nsp++;
+//         nst-=2;
+//         cout<<endl;
+//     }
+// }
+
 
